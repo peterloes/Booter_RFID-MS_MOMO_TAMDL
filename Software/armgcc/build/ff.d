051fb081b6f678329a3c0485ff59a30a48fd2768
Peter Loes <54844299+peterloes@users.noreply.github.com>
2019-10-08 14:13:08 +0200

    Add files via upload

---
+++ Software/armgcc/build/ff.d
@@ -0,0 +1,10 @@
+build/ff.o: ../fatfs/src/ff.c ../fatfs/inc/ff.h ../fatfs/inc/integer.h \
+ ../ffconf.h ../fatfs/inc/diskio.h
+
+../fatfs/inc/ff.h:
+
+../fatfs/inc/integer.h:
+
+../ffconf.h:
+
+../fatfs/inc/diskio.h:
